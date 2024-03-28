c9c41fcccf0c2f51b2b5f1061ac30e4b7ad655cd
tushar-autovyn <tushar.sharma@autovyn.in>
2024-03-28 10:59:48 +0530

    building error solve

--- src/app/module/dashboard/landing-page/home/home.component.ts
+++ src/app/module/dashboard/landing-page/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss', '../../../default.scss']
 })
 export class HomeComponent {
-
+  @Input() isLoading: boolean = false;
 }
